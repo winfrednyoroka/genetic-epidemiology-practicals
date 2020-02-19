055668df4eba706b794d0f5335f511b7b51540ef
epxlp <l.paternoster@bristol.ac.uk>
2020-02-19 15:10:24 +0000

    update paths

--- GWAS/scripts/qc.sh
+++ GWAS/scripts/qc.sh
@@ -3,11 +3,11 @@
 module load apps/plink2
 
 plink \
- 	--bfile ../data/geno_unclean \
+ 	--bfile ${datadir}/geno_unclean \
  	--maf 0.01 \
  	--hwe 1e-6 \
  	--geno 0.05 \
  	--mind 0.05 \
  	--make-bed \
- 	--out ../data/geno_qc
+ 	--out ~/genetic-epidemiology-practicals/GWAS/data/geno_qc
 
