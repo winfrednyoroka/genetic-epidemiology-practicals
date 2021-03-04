9ceb8ad432f8f908a138ef92aa0bc6be85ee3e11
Josine Min <josine.min@bristol.ac.uk>
2021-03-04 14:14:46 +0000

    updated GWAS scripts

--- GWAS/scripts/unclean_gwas.sh
+++ GWAS/scripts/unclean_gwas.sh
@@ -4,12 +4,12 @@ module load apps/plink2
 datadir="/newshared/genetic.epidemiology/GWAS/data"
 
 plink \
-	--bfile ${datadir}/geno_unclean \
-	--linear \
-	--pheno ${datadir}/phen.txt \
-	--pheno-name BMI \
-	--covar ${datadir}/covs.txt \
-	--covar-name age sex \
-	--out ~/genetic-epidemiology-practicals/GWAS/output/bmi       
+        --bfile ${datadir}/geno_unclean \
+        --linear \
+        --pheno ${datadir}/phen.txt \
+        --pheno-name BMI \
+        --covar ${datadir}/covs.txt \
+        --covar-name age sex \
+        --out ~/genetic-epidemiology-practicals/GWAS/output/bmi
 
 awk 'NR==1 || /ADD/' ~/genetic-epidemiology-practicals/GWAS/output/bmi.assoc.linear > ~/genetic-epidemiology-practicals/GWAS/output/bmi.assoc.linear.add
