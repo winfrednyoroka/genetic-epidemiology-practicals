9e76f5bb6880704312eeb6c1d1a7a5c5a7ed9935
abudu-aggrey <ashley.budu-aggrey@bristol.ac.uk>
2022-06-21 22:42:20 +0100

    Update Script1_file_prep.r
    Updated for BC4

--- PolygenicScores/scripts/Script1_file_prep.r
+++ PolygenicScores/scripts/Script1_file_prep.r
@@ -3,7 +3,7 @@
 ################################################################################################################
 
 # Change your working directory (you may need to change the filepath in next line)
-setwd("~/genetic-epidemiology-practicals/PolygenicScores/data")
+setwd("~/scratch/genetic-epidemiology-practicals/PolygenicScores/data")
 
 # Open discovery sample file with 180,290 SNPs from PGC schizophrenia meta-analysis
 SCZdata <- read.table("pgc_scz_gwas_summary_statistics.txt", header=T)
@@ -36,7 +36,7 @@ write.table(subset(SCZdata, select=c("SNP","P")), file="effect_allele_pvalue.txt
 ################################################################################################################
 
 # Change your working directory (you may need to change the filepath in next line)
-setwd("~/genetic-epidemiology-practicals/PolygenicScores/data")
+setwd("~/scratch/genetic-epidemiology-practicals/PolygenicScores/data")
 
 # Explore the phenotypic data (this data set includes a BMI variable as well as blood pressure, 
 # hypertension and C-reactive protein measures):
@@ -45,7 +45,7 @@ outcomeData <- read.table("outcome.txt", header=T)
 # Examine the distribution of the BMI variable
 summary(outcomeData$bmi)
 # visualise the distribution as a histogram and save as jpeg
-jpeg(file="~/genetic-epidemiology-practicals/PolygenicScores/results/hist_bmi.jpeg")
+jpeg(file="~/scratch/genetic-epidemiology-practicals/PolygenicScores/results/hist_bmi.jpeg")
 hist(outcomeData$bmi, breaks=40, xlim=c(0,max(outcomeData$bmi)), col="lightgrey")
 dev.off()
 
@@ -55,7 +55,7 @@ outcomeData$bmi[outcomeData$bmi>100 | outcomeData$bmi<10] <- NA
 # Examine the distribution of the BMI variable after removing the outliers: 
 summary(outcomeData$bmi)
 # Again, visualise the distribution as a histogram and save as jpeg
-jpeg(file="~/genetic-epidemiology-practicals/PolygenicScores/results/hist_bmi_clean.jpeg")
+jpeg(file="~/scratch/genetic-epidemiology-practicals/PolygenicScores/results/hist_bmi_clean.jpeg")
 hist(outcomeData$bmi, breaks=40, xlim=c(0,max(outcomeData$bmi, na.rm=TRUE)), col="lightgrey")
 dev.off()
 
